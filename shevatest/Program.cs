1c88377dd6317bcf1a08c9510300009ca1de71c1
anakib1 <b.svjatoslav@gmail.com>
2022-09-23 11:29:47 +0300

    Добавьте файлы проекта.

---
+++ shevatest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace shevatest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("You entered value : {0}", n);
+        }
+    }
+}
---
+++ shevatestform/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace shevatestform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(String.Format("You entered number {0}", this.textBox1.Text));
+        }
+    }
+}
